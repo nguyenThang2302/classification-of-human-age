0b4385f4dbd44b794ea1b01ea11c72ac465d51a6
er-thangnv <thangnv@tech.est-rouge.com>
2024-12-12 14:53:33 +0700

    HMAC-17: Implement get history images

--- Website/src/services/media/getHistoryImageList.ts
+++ Website/src/services/media/getHistoryImageList.ts
@@ -1,10 +1,9 @@
 import { instance } from '../instance';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
 export const fetchHistoryImages = async (offset: any, limit: any) => {
   try {
-    const accessToken = await AsyncStorage.getItem('access_token');
+    const accessToken = await localStorage.getItem('access_token');
     const response = await instance.get(`media/images?offset=${offset}&limit=${limit}`, {
       headers: {
         Authorization: `Bearer ${accessToken}`,
